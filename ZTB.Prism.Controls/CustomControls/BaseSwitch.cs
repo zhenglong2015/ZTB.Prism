7802562e580e499d593ef0b681bd8ceb0b881060
少林扫地僧 <853222835@qq.com>
2018-06-09 11:25:44 +0800

    增加radioButton样式

---
+++ ZTB.Prism.Controls/CustomControls/BaseSwitch.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZTB.Prism.Controls.CustomControls
+{
+    
+    public class BaseSwitch : CheckBox
+    {
+        public BaseSwitch()
+        {
+            this.Style = Application.Current.Resources["BaseSwitch"] as Style;
+        }
+
+
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+           "Text", typeof(string), typeof(BaseSwitch), new PropertyMetadata("Off"));
+        /// <summary>
+        /// 默认文本（未选中）
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty CheckedTextProperty = DependencyProperty.Register(
+            "CheckedText", typeof(string), typeof(BaseSwitch), new PropertyMetadata("On"));
+        /// <summary>
+        /// 选中状态文本
+        /// </summary>
+        public string CheckedText
+        {
+            get { return (string)GetValue(CheckedTextProperty); }
+            set { SetValue(CheckedTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty CheckedForegroundProperty =
+            DependencyProperty.Register("CheckedForeground", typeof(Brush), typeof(BaseSwitch), new PropertyMetadata(Brushes.WhiteSmoke));
+        /// <summary>
+        /// 选中状态前景样式
+        /// </summary>
+        public Brush CheckedForeground
+        {
+            get { return (Brush)GetValue(CheckedForegroundProperty); }
+            set { SetValue(CheckedForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty CheckedBackgroundProperty =
+            DependencyProperty.Register("CheckedBackground", typeof(Brush), typeof(BaseSwitch), new PropertyMetadata(Brushes.LimeGreen));
+        /// <summary>
+        /// 选中状态背景色
+        /// </summary>
+        public Brush CheckedBackground
+        {
+            get { return (Brush)GetValue(CheckedBackgroundProperty); }
+            set { SetValue(CheckedBackgroundProperty, value); }
+        }
+    }
+}
